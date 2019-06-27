20ec0760fb9de3516502822bf90960c50ce0469a
Sachin Samarasinghe <wills4705@gmail.com>
2019-06-27 18:08:29 -0400

    Added libraries

---
+++ scripts/index-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('#loader-container').fadeOut(1500);
+
+  $('#btn-about-me').click(function() {
+    $('.content').css('overflow', 'auto');
+    $('#about').fadeIn(1500);
+    document.getElementById('about').scrollIntoView(true);
+  });
+
+  $('#btn-skills').click(function() {
+    $('#skills').fadeIn(1500);
+    document.getElementById('skills').scrollIntoView(true);
+  })
+});
---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+$(function () {
+  $(".fade-in").css("opacity", "1");
+  $(".fade-in").css("left", "0");
+  $(".fade-in").css("top", "0");
+});
