2bd080a2fea5cfb8ed55ede726ef1233b42563f3
Sachin Samarasinghe <wills4705@gmail.com>
2019-07-29 18:05:02 -0400

    reveresed refatoring

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -91,31 +91,42 @@ $(document).ready(function () {
   //Part below handles the validation (including captcha) for the contact me form
   $("#contactMeForm").submit(function(e){
     e.preventDefault();
-    var t = grecaptcha.getResponse();
-    if (!captchaSucess || !validateText(t)) {
-      displayModal('Error!', 'Please complete the captcha challenge', 'text-danger');
-      return;
+    if ($("#message").val().length > 5000) {
+      displayModal('Error!', 'Length of the message is longer than 5000 characters',
+     'text-danger');
+     return;
     }
+    var t = grecaptcha.getResponse();
+    // if (!captchaSucess || !validateText(t)) {
+    //   displayModal('Error!', 'Please complete the captcha challenge', 'text-danger');
+    //   return;
+    // }
     $.post('post',
     {
       name: $('#name').val(),
       email: $('#email').val(),
       subject: $('#subject').val(),
-      message: $('#message').val(),
-      token: t
+      message: $('#message').val()
+      // token: t
     }).done(function(data,status) {
       if (data.code == 0) {
         displayModal('Success', 'Message sent successfully!', 'text-success');
         grecaptcha.reset();
       }
+      captchaSucess = false;
     }).fail(function (xhr, status, error) {
       if (xhr.status == 401) {
         displayModal('Error!', 'Please complete the captcha challenge', 'text-danger');
       } else if (xhr.status == 500) {
         displayModal('Error!', error, 'text-danger');
-      } else {
+      } else if(xhr.status == 413) {
+        displayModal('Error!', 'Length of the message is longer than 5000 characters',
+       'text-danger');
+      }
+      else {
         displayModal('Error!', 'Couldn\'t send the message', 'text-danger');
       }
-    }).finally(function () {captchaSucess = false;});
+      captchaSucess = false;
+    });
   });
 });
